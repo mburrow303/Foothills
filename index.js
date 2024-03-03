fb7ae37ffc8e8abd6024bba7f3d3b5c299913742
mburrow303 <elio21@comcast.net>
2024-03-03 13:43:16 -0700

    index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const { PORT, MONGO } = process.env;
+mongoose.connect(`${process.env.MONGO}/Foothills`);
+const db = mongoose.connection;
+db.once("open", () => console.log(`Connected to: ${MONGO}`));
+
+app.use(express.json());
+app.use(require("cors")());
+
+app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
